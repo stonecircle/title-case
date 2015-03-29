752498211a0dd75fdb9e615e7b4262065ad44da0
Blake Embrey <hello@blakeembrey.com>
2015-03-29 16:11:59 -0300

    update to standard style

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
 /* global describe, it */
-var assert    = require('assert');
-var titleCase = require('./');
+var assert = require('assert')
+var titleCase = require('./')
 
 describe('title case', function () {
   it('should title case a single word', function () {
-    assert.equal(titleCase('test'), 'Test');
-    assert.equal(titleCase('TEST'), 'Test');
-  });
+    assert.equal(titleCase('test'), 'Test')
+    assert.equal(titleCase('TEST'), 'Test')
+  })
 
   it('should title case regular sentence cased strings', function () {
-    assert.equal(titleCase('test string'), 'Test String');
-    assert.equal(titleCase('Test String'), 'Test String');
-  });
+    assert.equal(titleCase('test string'), 'Test String')
+    assert.equal(titleCase('Test String'), 'Test String')
+  })
 
   it('should title case non-alphanumeric separators', function () {
-    assert.equal(titleCase('dot.case'), 'Dot Case');
-    assert.equal(titleCase('path/case'), 'Path Case');
-  });
+    assert.equal(titleCase('dot.case'), 'Dot Case')
+    assert.equal(titleCase('path/case'), 'Path Case')
+  })
 
   it('should title case pascal cased strings', function () {
-    assert.equal(titleCase('TestString'), 'Test String');
-    assert.equal(titleCase('TestString1_2_3'), 'Test String 1 2 3');
-  });
+    assert.equal(titleCase('TestString'), 'Test String')
+    assert.equal(titleCase('TestString1_2_3'), 'Test String 1 2 3')
+  })
 
   it('should support non-ascii characters', function () {
-    assert.equal(titleCase('éxample'), 'Éxample');
-  });
+    assert.equal(titleCase('éxample'), 'Éxample')
+  })
 
   it('should support locales', function () {
-    assert.equal(titleCase('STRING', 'tr'), 'Strıng');
-  });
-});
+    assert.equal(titleCase('STRING', 'tr'), 'Strıng')
+  })
+})
